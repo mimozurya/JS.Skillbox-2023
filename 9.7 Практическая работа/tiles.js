692b9c838e4f23109287933a003caa2c5f318b0a
mimozurya <egor_sidorenko007@mail.ru>
2023-10-30 21:17:59 +0500

    загрузил 8 домашнее задание

--- 9.7 Практическая работа/tiles.js
+++ 9.7 Практическая работа/tiles.js
@@ -60,7 +60,7 @@
                                     tempVariable.classList.add('close');
                                     index = 0;
                                     },
-                                    800
+                                    500
                                 );
                             }
                         }
@@ -77,11 +77,52 @@
         button.textContent = 'Сыграть еще раз';
         button.classList.add('btn');
         document.body.append(button);
+        let input = document.querySelector('.input');
+        input.value = 'Успел!';
 
         button.onclick = function () {
             window.location.reload();
         }
     }
 
+    function createNumbers () {
+        let form = document.querySelector('.form');
+        let input = document.createElement('input');
+        let text = document.querySelector('.text');
+        input.classList.add('input');
+        form.append(input);
+        
+        input.onchange = function () {
+            if (Number(input.value) > 1 && Number(input.value) < 11 && Number(input.value) % 2 === 0) {
+                startGame(document.getElementById('signs'), Number(input.value));
+
+                text.textContent = 'Осталось времени:';
+                input.value = 60;
+                var counter = setInterval(timer, 1000);
+                function timer () {
+                    if (!isNaN(input.value))
+                    input.value = input.value - 1;
+                    if (input.value <= 0 && input.value !== 'Успел!') {
+                        clearInterval(counter); 
+                        input.value = 'Вы не успели!';
+                        setTimeout (() => {
+                            window.location.reload();
+                            },
+                            3000
+                        );
+                    }
+                }
+
+            } else if (Number(input.value) % 2 !== 0) {
+                input.value = '';
+                input.placeholder = 'Чётное!';
+            } else {
+                input.value = '';
+                input.placeholder = 'Значение от 2 до 10!';
+            }
+        };
+    }
+
     window.startGame = startGame;
+    window.createNumbers = createNumbers;
 })();
